e15d49cf8c0f486ff3692b456f480b7d3fb0abde
Kalia Thao <kaliathao21@gmail.com>
2023-11-19 18:35:56 -0800

    Edited mutations in typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -22,8 +22,12 @@ type Query {
 }
 
 type Mutation {
-    addBook(authors: String!, description: String!, bookId: String!, image: String!, link: String!, title: String!): Book
+    login(email: String!, passworkd: String!): Auth
+    addUser(userName: String!, email: String!, password: String!): Auth
+    saveBook(authors: [String!], description: String!, title: String!, bookId: String!, image: String!, link: String!)
+    removeBook(bookId: String!)
 }
+
 `;
 
 module.exports = typeDefs;
